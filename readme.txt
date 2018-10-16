daff60768ef51e3ea314a7093552252e4de3fdb6
mikhavia <Viatcheslav.Mikhalev@vermop.com>
2018-10-16 21:20:54 +0200

    simpleThymeleafInternation initial commit

--- readme.txt
+++ readme.txt
@@ -2,4 +2,5 @@ simpleThymeleafGuiInMemory
 simpleThymeleafGuiInMemoryDB -> Extended simpleThymeleafGuiInMemory implementation 
 simpleThymeleafGuiSearch -> Extended simpleThymeleafGuiInMemoryDB implementation
 simpleThymeleafPaging -> Extended simpleThymeleafGuiSearch implementation
+simpleThymeleafInternation -> Extended simpleThymeleafPaging implementation
 ....
---
+++ simpleThymeleafInternation/src/main/java/de/homedev/thymeleaf/simplegui/model/PersonEntity.java
@@ -0,0 +1,78 @@
+package de.homedev.thymeleaf.simplegui.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+import javax.persistence.Version;
+
+@Entity
+@Access(AccessType.FIELD)
+@Table(name = PersonEntity.TABLE_NAME)
+public class PersonEntity implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	public static final String TABLE_NAME = "person";
+
+	@Id
+	@TableGenerator(name = TABLE_NAME, table = "bt_sequences", pkColumnName = "sequencename", valueColumnName = "sequencevalue", pkColumnValue = TABLE_NAME, initialValue = 1, allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.TABLE, generator = TABLE_NAME)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Version
+	@Column(name = "version", nullable = false)
+	private Integer version;
+
+	public PersonEntity() {
+
+	}
+
+	public PersonEntity(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+}
